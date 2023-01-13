a99216a3c7cb2f875d14fe5634af4a4198adc06c
Tori Enyart <torienyart@gmail.com>
2023-01-13 09:25:33 -0700

    feat: Add function to add and track expenses

--- lib/department.rb
+++ lib/department.rb
@@ -1,12 +1,17 @@
 class Department
-  attr_reader :name, :employees
+  attr_reader :name, :employees, :expenses
   def initialize(name)
     @name = name
     @employees = []
+    @expenses = 0
   end
 
   def hire(employee)
     @employees << employee
+  end
+
+  def expense(amount)
+    @expenses += amount
   end
 
 end

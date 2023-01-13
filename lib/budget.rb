7371fa156e2238954d9dda58c45ccf9972acfec6
Tori Enyart <torienyart@gmail.com>
2023-01-13 09:49:22 -0700

    feat: Create functionality for adding and storing departments

--- lib/budget.rb
+++ lib/budget.rb
@@ -1,8 +1,14 @@
 class Budget
-  attr_reader :year
+  attr_reader :year, :departments
 
   def initialize(year)
     @year = year
+    @departments = []
   end
 
+  def add_department(dep)
+    @departments << dep
+  end
+
+  
 end

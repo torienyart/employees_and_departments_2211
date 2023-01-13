81a280de75fc6027411a73c71e9d77b6a3133f0c
Tori Enyart <torienyart@gmail.com>
2023-01-13 10:18:41 -0700

    test: Write tests for Expense class

---
+++ spec/expense_spec.rb
@@ -0,0 +1,23 @@
+require './lib/expense'
+require './lib/employee'
+require './lib/department'
+
+
+
+describe Expense do
+  let(:bobbi){Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
+  let(:customer_service){Department.new("Customer Service")}
+  let(:business_cards){customer_service.expense(bobbi, 50, 'business cards')}
+
+  before(:each) do
+    customer_service.hire(bobbi)
+  end
+
+  it 'exists and has readable attributes' do 
+    expect(business_cards).to be_an_instance_of Expense
+    expect(business_cards.item).to eq('business cards')
+    expect(business_cards.spender).to eq(bobbi)
+    expect(business_cards.cost).to eq(50)
+  end
+
+end

715461dfd1c4846a8cb34cc32bcf1a336ed4d2d8
Tori Enyart <torienyart@gmail.com>
2023-01-13 09:12:40 -0700

    feat: Initialize Employee and format salary

---
+++ lib/employee.rb
@@ -0,0 +1,12 @@
+class Employee
+  attr_reader :name, :age, :salary
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age].to_i
+    @salary = salary_converter(info[:salary])
+  end
+
+  def salary_converter(info)
+    info.scan(/\d/).join('').to_i
+  end
+end
